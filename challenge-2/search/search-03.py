5e61cef3771d167489a131b8bc8c12a14dc2ee1d
Ankit Raj <rajankit749@gmail.com>
2022-11-15 16:20:46 +0000

    reorg and commit  all files

---
+++ challenge-2/search/search-03.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+url = "https://searchwebdocs.search.windows.net/indexes/webdocs-index/docs/search?api-version=2021-04-30-Preview&search
+"
+
+payload = json.dumps({
+  "search": "content:Las Vegas AND reviews~",
+  "queryType": "full",
+  "searchMode": "all",
+  "select": "file_name, url, metadata_storage_size,metadata_creation_date",
+  "count": "true"
+})
+headers = {
+  'Content-Type': 'application/json',
+  'api-key': 'your_api_key'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
---
+++ challenge-2/search/search-04.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+url = "https://searchwebdocs.search.windows.net/indexes/webdocs-index/docs/search?api-version=2021-04-30-Preview&search
+"
+
+payload = json.dumps({
+  "search": "content:Las Vegas NOT reviews~",
+  "queryType": "full",
+  "searchMode": "all",
+  "searchFields": "content",
+  "select": "file_name, url, metadata_storage_size,metadata_creation_date",
+  "count": "true"
+})
+headers = {
+  'Content-Type': 'application/json',
+  'api-key': 'your_api_key'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
